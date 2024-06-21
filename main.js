3e83a203a2ea78c48c74320462a14a7b5c11180c
hellanerdkidfromthehood <ronilamos@gmail.com>
2024-06-21 21:18:19 +0800

    added new model object

--- main.js
+++ main.js
@@ -4,20 +4,27 @@ import { loadGLTF } from './loader.js';
 
 const mindarThree = new MindARThree({
 	container: document.querySelector("#container"),
-	imageTargetSrc: "./assets/targets/musicband.mind"
+	imageTargetSrc: "./assets/targets/planets.mind"
 });
   
   const {renderer, scene, camera} = mindarThree;
-  
+
   const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-  const raccoon = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
-    raccoon.scene.scale.set(0.1, 0.1, 0.1);
-    raccoon.scene.position.set(0, -0.4, 0);
+  const earth = await loadGLTF('../../assets/models/earth/scene.gltf');
+    earth.scene.scale.set(0.1, 0.1, 0.1);
+    earth.scene.position.set(0, -0.4, 0);
 
-  const raccoonAnchor = mindarThree.addAnchor(0);
-    raccoonAnchor.group.add(raccoon.scene);
+  const earthAnchor = mindarThree.addAnchor(0);
+    earthAnchor.group.add(earth.scene);
+
+  const jupiter = await loadGLTF('../../assets/models/jupiter/scene.gltf');
+  jupiter.scene.scale.set(0.1, 0.1, 0.1);
+  jupiter.scene.position.set(0, -0.4, 0);
+
+  const jupiterAnchor = mindarThree.addAnchor(0);
+  jupiterAnchor.group.add(jupiter.scene);
   
   const start = async() => {
 	  await mindarThree.start();
