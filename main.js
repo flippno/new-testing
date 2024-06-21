4439b1701056bdf275ab92884c873f2bb2b0adc2
hellanerdkidfromthehood <ronilamos@gmail.com>
2024-06-21 19:56:50 +0800

    new commits

---
+++ main.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+import { MindARThree } from 'mindar-image-three';
+import { loadGLTF } from './loader.js';
+
+const mindarThree = new MindARThree({
+	container: document.querySelector("#container"),
+	imageTargetSrc: "./assets/targets/musicband.mind"
+});
+  
+  const {renderer, scene, camera} = mindarThree;
+  const anchor = mindarThree.addAnchor(0);
+  const geometry = new THREE.PlaneGeometry(1, 0.55);
+  const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
+  const plane = new THREE.Mesh( geometry, material );
+  
+  anchor.group.add(plane);
+  
+  const start = async() => {
+	  await mindarThree.start();
+      renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+      });
+    }
+
+  const startButton = document.querySelector("#startButton");
+    
+  startButton.addEventListener("click", () => {
+	  start();
+  });
+  
+  stopButton.addEventListener("click", () => {
+    mindarThree.stop();
+    mindarThree.renderer.setAnimationLoop(null);
+  });
