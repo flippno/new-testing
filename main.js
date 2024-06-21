c8a9bbc730c3dbcf3cc83660bbab3e8ffbfec5cd
hellanerdkidfromthehood <ronilamos@gmail.com>
2024-06-22 01:33:34 +0800

    added new models

--- main.js
+++ main.js
@@ -13,18 +13,60 @@ const mindarThree = new MindARThree({
     scene.add(light);
 
   const earth = await loadGLTF('../../assets/models/earth/scene.gltf');
-    earth.scene.scale.set(0.1, 0.1, 0.1);
+    earth.scene.scale.set(0.3, 0.3, 0.3);
     earth.scene.position.set(0, -0.4, 0);
 
   const earthAnchor = mindarThree.addAnchor(0);
     earthAnchor.group.add(earth.scene);
 
   const jupiter = await loadGLTF('../../assets/models/jupiter/scene.gltf');
-  jupiter.scene.scale.set(0.1, 0.1, 0.1);
-  jupiter.scene.position.set(0, -0.4, 0);
+    jupiter.scene.scale.set(0.3, 0.3, 0.3);
+    jupiter.scene.position.set(0, -0.4, 0);
 
   const jupiterAnchor = mindarThree.addAnchor(1);
-  jupiterAnchor.group.add(jupiter.scene);
+    jupiterAnchor.group.add(jupiter.scene);
+
+  const mars = await loadGLTF('../../assets/models/mars/scene.gltf');
+    mars.scene.scale.set(0.1, 0.1, 0.1);
+    mars.scene.position.set(0, -0.4, 0);
+
+  const marsAnchor = mindarThree.addAnchor(2);
+    marsAnchor.group.add(mars.scene);
+
+  const mercury = await loadGLTF('../../assets/models/mercury/scene.gltf');
+    mercury.scene.scale.set(0.3, 0.3, 0.3);
+    mercury.scene.position.set(0, -0.4, 0);
+
+  const mercuryAnchor = mindarThree.addAnchor(3);
+    mercuryAnchor.group.add(mercury.scene);
+
+  const neptune = await loadGLTF('../../assets/models/neptune/scene.gltf');
+    neptune.scene.scale.set(0.1, 0.1, 0.1);
+    neptune.scene.position.set(0, -0.4, 0);
+
+  const neptuneAnchor = mindarThree.addAnchor(4);
+    neptuneAnchor.group.add(neptune.scene);
+
+  const saturn = await loadGLTF('../../assets/models/saturn/scene.gltf');
+    saturn.scene.scale.set(0.3, 0.3, 0.3);
+    saturn.scene.position.set(0, -0.4, 0);
+
+  const saturnAnchor = mindarThree.addAnchor(5);
+    saturnAnchor.group.add(saturn.scene);
+
+  const uranus = await loadGLTF('../../assets/models/uranus/scene.gltf');
+    uranus.scene.scale.set(0.3, 0.3, 0.3);
+    uranus.scene.position.set(0, -0.4, 0);
+
+  const uranusAnchor = mindarThree.addAnchor(6);
+    uranusAnchor.group.add(uranus.scene);
+
+  const venus = await loadGLTF('../../assets/models/venus/scene.gltf');
+    venus.scene.scale.set(0.3, 0.3, 0.3);
+    venus.scene.position.set(0, -0.4, 0);
+
+  const venusAnchor = mindarThree.addAnchor(7);
+    venusAnchor.group.add(venus.scene);
   
   const start = async() => {
 	  await mindarThree.start();
